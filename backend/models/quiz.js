bb0703dde9d45a26ad241a91508f57bd55cda357
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-08-12 23:19:59 +0200

    quiz model init

---
+++ backend/models/quiz.js
@@ -0,0 +1,40 @@
+/* jslint es6 */
+"global require";
+const mongoose = require("mongoose");
+
+const quizSchema = new mongoose.Schema({
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "Creator is required"],
+  },
+  //   subCategory: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "SubCategory",
+  //     required: [true, "Subcategory is required"],
+  //   },
+  createdAt: { type: Date, default: Date.now },
+  image: {
+    type: Buffer,
+  },
+  imageType: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+  },
+});
+
+quizSchema.virtual("imagePath").get(function () {
+  if (this.image != null && this.imageType != null) {
+    return `data:${this.imageType};charset=utf-8;base64,${this.image.toString(
+      "base64"
+    )}`;
+  }
+});
+
+module.exports = mongoose.model("Quiz", quizSchema);
