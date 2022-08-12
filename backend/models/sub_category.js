dd145d76b2c0377a540973c8814e1bff2864ef6b
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-08-12 23:20:24 +0200

    sub category model init

---
+++ backend/models/sub_category.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const subCategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  image: {
+    type: Buffer,
+    required: [true, "Cover image is required"],
+  },
+  imageType: {
+    type: String,
+    required: [true, "Cover image type is required"],
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: [true, "Category is required"],
+  },
+});
+
+subCategorySchema.virtual("imagePath").get(function () {
+  if (this.image != null && this.imageType != null) {
+    return `data:${this.imageType};charset=utf-8;base64,${this.image.toString(
+      "base64"
+    )}`;
+  }
+});
+
+module.exports = mongoose.model("SubCategory", subCategorySchema);
