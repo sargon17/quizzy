daf0370a0f06f844142ce0d0deaa632f11009d21
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-08-12 23:20:37 +0200

    category model init

---
+++ backend/models/category.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  image: {
+    type: Buffer,
+    required: [true, "Cover image is required"],
+  },
+  imageType: {
+    type: String,
+    required: [true, "Cover image type is required"],
+  },
+});
+
+categorySchema.virtual("imagePath").get(function () {
+  if (this.image != null && this.imageType != null) {
+    return `data:${this.imageType};charset=utf-8;base64,${this.image.toString(
+      "base64"
+    )}`;
+  }
+});
+
+module.exports = mongoose.model("Category", categorySchema);
