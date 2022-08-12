73e3a89b89328bf498779d1405d3bfebe2953931
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-08-12 23:21:21 +0200

    auth controller init

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -9,34 +9,40 @@ exports.signup = async (req, res) => {
     email: req.body.email,
     password: bcrypt.hashSync(req.body.password, 10),
   });
+  const token = jwt.sign({ _id: user._id }, process.env.API_SECRET, {
+    expiresIn: "12h",
+  });
   try {
     await user.save();
-    res.status(200).send({ message: "User created successfully!" });
+    res.status(200).json({
+      message: "User created successfully!",
+      user: user,
+      accessToken: token,
+    });
   } catch (err) {
     console.log(err);
-    res.status(500).send({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
 
 exports.signin = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email }).exec();
-    // if (!user) {
-    //   return res.status(401).send({ message: "Invalid email or password." });
-    // }
     if (!user) {
-      return res.status(401).send({ message: "Invalid email or password." });
+      return res.status(401).json({ message: "Invalid email or password." });
     }
     const passwordCheck = bcrypt.compareSync(req.body.password, user.password);
     if (!passwordCheck) {
       return res
         .status(401)
-        .send({ accessToken: null, message: "Invalid email or password." });
+        .json({ accessToken: null, message: "Invalid email or password." });
     }
+
     const token = jwt.sign({ _id: user._id }, process.env.API_SECRET, {
-      expiresIn: "1h",
+      expiresIn: "12h",
     });
-    res.status(200).send({
+
+    res.status(200).json({
       user: {
         _id: user._id,
         userName: user.userName,
@@ -46,6 +52,6 @@ exports.signin = async (req, res, next) => {
       message: "User signed in successfully!",
     });
   } catch (err) {
-    res.status(500).send({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
