45f7b77e5d689d26b7cb4fb2d2d277deba84679b
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-08-18 18:57:07 +0200

    grid cards component init

---
+++ frontend/src/components/GridCards.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+import "../scss/style.scss";
+
+export default function GridCards({ Data, linkPath }) {
+  return (
+    <div>
+      <ul className="flex flex-wrap justify-center gap-2 ">
+        {Data &&
+          Data.map((d) => {
+            return (
+              <li className="card card-horizontal" key={d.id}>
+                <div className="card-cover">
+                  <img src={d.image} />
+                </div>
+                <div className="card-body">
+                  <div className="card-content">
+                    <h4 className="card-title">{d.title}</h4>
+                  </div>
+                  <div className="card-footer flex justify-end">
+                    <Link
+                      className="btn btn-primary-outlined btn-round-s"
+                      to={`${linkPath}${d.id}`}
+                    >
+                      GO
+                    </Link>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
+}
