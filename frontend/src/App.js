18a2b14d8025ba57c97b758e75dd2619ab37d824
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-08-22 23:23:39 +0200

    controlling for user logging in app file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,44 @@
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import React from "react";
+import { useEffect, useState } from "react";
+
+import { useCookies } from "react-cookie";
+import checkUserAuth from "./utils/checkUserAuth";
 
 import Homepage from "./pages/Homepage";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import QuizControlPage from "./pages/QuizControlPage";
-import CategoryPage from "./pages/CategoryPage";
+import SubCategoryPage from "./pages/SubCategoriesPage";
+import QuizzesPage from "./pages/QuizzesPage";
+import QuizPage from "./pages/QuizPage";
 import "./App.css";
 
 function App() {
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userData, setUserData] = useState({ userName: "", email: "" });
+
+  useEffect(() => {
+    if (cookies.user && cookies.accessToken) {
+      if (checkUserAuth(cookies)) {
+        setIsAuthenticated(true);
+        setUserData({
+          userName: cookies.user.userName,
+          email: cookies.user.email,
+        });
+      }
+    }
+  }, []);
+
+  const logout = () => {
+    removeCookie("user", { path: "/" });
+    removeCookie("accessToken", { path: "/" });
+    window.location.href = "/login";
+  };
+
   return (
     <Router>
       <div className="App">
@@ -23,22 +53,47 @@ function App() {
                   Home
                 </Link>
               </li>
-              <li>
-                <Link
-                  to="/login"
-                  className="p-4 transition-all duration-300 hover:text-blue-500"
-                >
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/register"
-                  className="p-4 transition-all duration-300 hover:text-blue-500"
-                >
-                  Register
-                </Link>
-              </li>
+              {!isAuthenticated ? (
+                <>
+                  <li>
+                    <Link
+                      to="/login"
+                      className="p-4 transition-all duration-300 hover:text-blue-500"
+                    >
+                      Login
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/register"
+                      className="p-4 transition-all duration-300 hover:text-blue-500"
+                    >
+                      Register
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <p>Welcome {userData.userName}</p>
+                  <li>
+                    <Link
+                      to={`/dashboard/${userData.userName}`}
+                      className="p-4 transition-all duration-300 hover:text-blue-500"
+                    >
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="m-0 btn btn-secondary btn-xs btn-round-s "
+                      onClick={logout}
+                    >
+                      Logout
+                    </a>
+                  </li>
+                </>
+              )}
             </ul>
           </nav>
         </header>
@@ -47,7 +102,12 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard/:userName" element={<Dashboard />} />
-          <Route path="/category/:categoryID" element={<CategoryPage />} />
+          <Route path="/category/:categoryID" element={<SubCategoryPage />} />
+          <Route
+            path="/sub-category/:subCategoryID"
+            element={<QuizzesPage />}
+          />
+          <Route path="/quiz/:quizID" element={<QuizPage />} />
           <Route
             path="/quiz-controller/:quizID"
             element={<QuizControlPage />}
