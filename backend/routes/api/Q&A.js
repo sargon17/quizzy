8759ff6c67a558ae017c6a3e66e724e094c7be8b
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-08-22 23:23:04 +0200

    new routes done

--- backend/routes/api/Q&A.js
+++ backend/routes/api/Q&A.js
@@ -13,6 +13,7 @@ const {
   answersByQuestionId,
   addAnswer,
   deleteAnswer,
+  controlAnswers,
 } = require("../../controllers/answer.controller");
 
 const checkForPhoto = (req, res, next) => {
@@ -22,11 +23,13 @@ const checkForPhoto = (req, res, next) => {
     next();
   }
 };
+// http://localhost:5000/api/qa
 router.get("/:id", questionsByQuizId); // get questions by quiz id
 router.post("/:id", upload.single("image"), createQuestion); // create question
 router.delete("/:id", deleteQuestion); // delete question
 
 router.get("/answers/:id", answersByQuestionId); // get answers by question id
+router.post("/answers/control", controlAnswers); // control answers
 router.post("/answers/:id", addAnswer); // get answers by question id
 router.delete("/answers/:id", deleteAnswer); // delete answer
 
