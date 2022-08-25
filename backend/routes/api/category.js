342823bdb3d67e16d7dec459f571616e0864d917
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-08-25 22:56:03 +0200

    sub category and category routers upd

--- backend/routes/api/category.js
+++ backend/routes/api/category.js
@@ -6,9 +6,11 @@ const upload = require("../../utils/storage");
 const {
   allCategories,
   createCategory,
+  categoriesByTitle,
 } = require("../../controllers/category.controller");
 
 router.get("/", allCategories); // get all categories
+router.get("/title/:title", categoriesByTitle); // get categories by title
 router.post("/create", upload.single("image"), createCategory); // create category
 
 module.exports = router;
--- backend/routes/api/sub_category.js
+++ backend/routes/api/sub_category.js
@@ -7,10 +7,12 @@ const {
   getSubCategories,
   subCategoriesByCategory,
   createSubCategory,
+  subCategoriesByCategoryAndTitle,
 } = require("../../controllers/sub_category.controller");
 
 // router.get("/", getSubCategories);
 router.post("/create", upload.single("image"), createSubCategory);
 router.get("/:categoryID", subCategoriesByCategory);
+router.get("/title/:title/:categoryID", subCategoriesByCategoryAndTitle);
 
 module.exports = router;
