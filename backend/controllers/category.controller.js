f8f68a548c7ae9c08e4be036a2dafd08ceeab426
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-08-25 22:54:09 +0200

    added category search

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -34,6 +34,31 @@ exports.allCategories = async (req, res) => {
   }
 };
 
+exports.categoriesByTitle = async (req, res) => {
+  let query = Category.find();
+
+  if (req.params.title != null && req.params.title != "") {
+    query = query.regex("title", new RegExp(req.params.title, "i"));
+  }
+  try {
+    const c = await query.exec();
+
+    let categories = c.map((category) => {
+      return {
+        id: category._id,
+        title: category.title,
+        image: category.imagePath,
+      };
+    });
+    res.status(200).json({
+      message: "Categories retrieved successfully!",
+      categories: categories,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 exports.createCategory = async (req, res) => {
   const image = fs.readFileSync(req.file.path);
   const encode_image = image.toString("base64");
