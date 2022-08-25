90b7dfdd5672a70dd5539c3784d3a5ecfe00beea
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-08-25 22:53:46 +0200

    renamed file

--- frontend/src/components/manageQuiz.js
+++ frontend/src/components/manageQuiz.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 import LoadImage from "./loadImage";
 import AddCategory from "./AddCategory";
@@ -11,22 +11,52 @@ import { userDataSelector } from "../features/user/userDataSlice";
 
 import useOnclickOutside from "react-cool-onclickoutside";
 
-export default function CreateQuiz({ userID }) {
+export default function ManageQuiz({ isNewQuiz }) {
   const user = useSelector(userDataSelector);
   const [data, setData] = useState({
     title: "Quiz by " + user.userName,
-    categoryID: "",
-    subCategoryID: "",
-    creatorID: userID,
+    category: {},
+    subCategory: {},
+    creatorID: user._id,
     description: "",
   });
   const [image, setImage] = useState("");
 
-  const [categories, setCategories] = useState([]);
-  const [subCategories, setSubCategories] = useState([]);
-  const [quizzes, setQuizzes] = useState([]);
+  useEffect(() => {
+    console.log();
+  }, [image]);
 
-  // state of title input
+  const { quizID } = useParams();
+  console.log(quizID);
+
+  const getQuiz = () => {
+    axios
+      .get(`http://localhost:5000/api/quiz/${quizID}`)
+      .then((response) => {
+        console.log(response.data);
+        setData((prevState) => ({
+          ...prevState,
+          id: response.data.quiz.id,
+          title: response.data.quiz.title,
+          description: response.data.quiz.description,
+          subCategory: response.data.quiz.subCategory,
+          category: response.data.quiz.subCategory.category,
+          creatorID: response.data.quiz.creator._id,
+        }));
+        setImage(response.data.quiz.image);
+        console.log(response.data.quiz);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    if (!isNewQuiz && quizID) {
+      getQuiz();
+    }
+  }, []);
+
+  // // state of title input
   const [isTitleInput, setIsTitleInput] = useState(false);
   const titleRef = useOnclickOutside(() => {
     setIsTitleInput(false);
@@ -38,12 +68,24 @@ export default function CreateQuiz({ userID }) {
     setIsDescriptionInput(false);
   });
 
+  // display add category form
+  const [isAddCategory, setIsAddCategory] = useState(false);
+
+  const setCategoryAndSubCategory = (category, subCategory) => {
+    setData({ ...data, category, subCategory });
+  };
+
+  // const [categories, setCategories] = useState([]);
+  // const [subCategories, setSubCategories] = useState([]);
+  // const [quizzes, setQuizzes] = useState([]);
+
   const createQuiz = () => {
-    console.log(`User ${userID} is creating a quiz`);
+    console.log(`User ${user._id} is creating a quiz`);
+    console.log(user);
     const formData = new FormData();
     formData.append("title", data.title);
-    formData.append("categoryID", data.categoryID);
-    formData.append("subCategoryID", data.subCategoryID);
+    formData.append("categoryID", data.category.id);
+    formData.append("subCategoryID", data.subCategory.id);
     formData.append("creatorID", data.creatorID);
     formData.append("description", data.description);
     formData.append("image", image[0].file);
@@ -57,67 +99,72 @@ export default function CreateQuiz({ userID }) {
       })
       .then((response) => {
         console.log(`Quiz created successfully -- ${response.data}`);
-        getQuizzes();
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
-  const getQuizzes = () => {
-    axios
-      .get(`http://localhost:5000/api/quiz/all/${userID}`)
-      .then((response) => {
-        setQuizzes(response.data.quizzes);
+  // const getQuizzes = () => {
+  //   axios
+  //     .get(`http://localhost:5000/api/quiz/all/${userID}`)
+  //     .then((response) => {
+  //       setQuizzes(response.data.quizzes);
 
-        console.log(response.data.quizzes);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+  //       console.log(response.data.quizzes);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // };
 
-  const getCategories = () => {
-    axios
-      .get("http://localhost:5000/api/category")
-      .then((response) => {
-        setData({ ...data, categoryID: response.data.categories[0].id });
-        console.log(data);
-        setCategories(response.data.categories);
-        console.log(response.data.categories);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  const getSubCategories = () => {
-    axios
-      .get(`http://localhost:5000/api/sub_category/${data.categoryID}`)
-      .then((response) => {
-        setSubCategories(response.data.subCategories);
-        setData({ ...data, subCategoryID: response.data.subCategories[0].id });
-        // console.log(response.data.subCategories);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+  // const getCategories = () => {
+  //   axios
+  //     .get("http://localhost:5000/api/category")
+  //     .then((response) => {
+  //       setData({ ...data, categoryID: response.data.categories[0].id });
+  //       console.log(data);
+  //       setCategories(response.data.categories);
+  //       console.log(response.data.categories);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // };
+  // const getSubCategories = () => {
+  //   axios
+  //     .get(`http://localhost:5000/api/sub_category/${data.categoryID}`)
+  //     .then((response) => {
+  //       setSubCategories(response.data.subCategories);
+  //       setData({ ...data, subCategoryID: response.data.subCategories[0].id });
+  //       // console.log(response.data.subCategories);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // };
 
-  useEffect(() => {
-    getCategories();
-    getSubCategories();
-    getQuizzes();
-  }, []);
+  // useEffect(() => {
+  //   getCategories();
+  //   getSubCategories();
+  //   getQuizzes();
+  // }, []);
 
-  useEffect(() => {
-    getSubCategories();
-  }, [data.categoryID]);
+  // useEffect(() => {
+  //   getSubCategories();
+  // }, [data.categoryID]);
 
   return (
-    <div className="create-quiz relative">
+    <div className="create-quiz">
       <div className="quiz-data grid grid-cols-4 grid-rows-1 gap-5 h-96">
         <div className="quiz-cover col-span-1 col-start-1 ">
-          <LoadImage setImage={setImage} />
+          {isNewQuiz ? (
+            <LoadImage setImage={setImage} />
+          ) : (
+            <div className="image-upload">
+              <img src={image} alt="quiz cover" />
+            </div>
+          )}
         </div>
         <div className="quiz-content col-span-3 col-start-2 flex flex-col h-full">
           <div className="quiz-title--wrapper">
@@ -150,10 +197,25 @@ export default function CreateQuiz({ userID }) {
               </>
             )}
           </div>
-          <div className="quiz-category">+ Add Category</div>
-          <div className=" absolute  ">
-            <AddCategory setData={setData} />
+          <div className="quiz-category" onClick={() => setIsAddCategory(true)}>
+            {!data.category.title && !data.subCategory.title ? (
+              <p>+ Add Category</p>
+            ) : (
+              <p>
+                {data.category.title}/{data.subCategory.title}
+              </p>
+            )}
           </div>
+          {isAddCategory && (
+            <div className="add-category--wrapper">
+              <AddCategory
+                setData={setCategoryAndSubCategory}
+                open={setIsAddCategory}
+                category={data.category}
+                subCategory={data.subCategory}
+              />
+            </div>
+          )}
           <div className="quiz-description--wrapper grow">
             {isDescriptionInput ? (
               <textarea
@@ -189,7 +251,10 @@ export default function CreateQuiz({ userID }) {
           <div className="quiz-content__footer flex justify-between w-full">
             <div>Add Special Rules</div>
             <div>
-              <button className="btn btn-primary btn-primary--shadow btn-sm btn-round-s m-0">
+              <button
+                className="btn btn-primary btn-primary--shadow btn-sm btn-round-s m-0"
+                onClick={createQuiz}
+              >
                 SAVE
               </button>
             </div>
