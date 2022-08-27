e8c05ff14cfb4ed319f47f5c3e73622c6d9c3a41
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-08-28 00:11:29 +0200

    quiz updated schema

--- backend/models/quiz.js
+++ backend/models/quiz.js
@@ -2,32 +2,37 @@
 "global require";
 const mongoose = require("mongoose");
 
-const quizSchema = new mongoose.Schema({
-  creator: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, "Creator is required"],
+const quizSchema = new mongoose.Schema(
+  {
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Creator is required"],
+    },
+    subCategory: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "SubCategory",
+      required: [true, "Subcategory is required"],
+    },
+    createdAt: { type: Date, default: Date.now },
+    image: {
+      type: Buffer,
+    },
+    imageType: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
   },
-  subCategory: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "SubCategory",
-    required: [true, "Subcategory is required"],
-  },
-  createdAt: { type: Date, default: Date.now },
-  image: {
-    type: Buffer,
-  },
-  imageType: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  title: {
-    type: String,
-    required: [true, "Title is required"],
-  },
-});
+  {
+    toJSON: { virtuals: true },
+  }
+);
 
 quizSchema.virtual("imagePath").get(function () {
   if (this.image != null && this.imageType != null) {
