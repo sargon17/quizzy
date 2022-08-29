92afc6427b63c1af68cbf6a526fa0dd513537eb1
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-08-29 23:51:26 +0200

    save image upg

--- backend/utils/saveImage.js
+++ backend/utils/saveImage.js
@@ -1,11 +1,12 @@
 const imageMimeType = ["image/jpeg", "image/png", "image/gif"];
 function saveImage(entity, imageEncoded) {
-  if (imageEncoded == null) return;
-  const image = imageEncoded.split(";base64,").pop();
-  // const image = JSON.parse(imageEncoded);
-  if (image != null && imageMimeType.includes(image.type)) {
-    entity.image = new Buffer.from(image.data, "base64");
-    entity.imageType = image.type;
+  if (imageEncoded !== null) {
+    const image = imageEncoded.split(";base64,").pop();
+    // const image = JSON.parse(imageEncoded);
+    if (image != null && imageMimeType.includes(image.type)) {
+      entity.image = new Buffer.from(image.data, "base64");
+      entity.imageType = image.type;
+    }
   }
 }
 
