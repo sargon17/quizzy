bae06d605fc3fcb4e469e0d1bbf52916271a6819
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-09-01 18:51:41 +0200

    category schema upd

--- backend/models/category.js
+++ backend/models/category.js
@@ -2,21 +2,26 @@ const mongoose = require("mongoose");
 
 const SubCategory = require("./sub_category");
 
-const categorySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Name is required"],
+const categorySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    image: {
+      type: Buffer,
+      required: [true, "Cover image is required"],
+    },
+    imageType: {
+      type: String,
+      required: [true, "Cover image type is required"],
+    },
+    subCategories: [],
   },
-  image: {
-    type: Buffer,
-    required: [true, "Cover image is required"],
-  },
-  imageType: {
-    type: String,
-    required: [true, "Cover image type is required"],
-  },
-  subCategories: [],
-});
+  {
+    toJSON: { virtuals: true },
+  }
+);
 
 categorySchema.virtual("imagePath").get(function () {
   if (this.image != null && this.imageType != null) {
