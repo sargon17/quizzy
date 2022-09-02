7aaaae5a3aa14d7b6f3c511fdc5b12d0a8545066
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-09-02 14:53:35 +0200

    category by id Api done

--- backend/routes/api/category.js
+++ backend/routes/api/category.js
@@ -8,9 +8,11 @@ const {
   createCategory,
   categoriesByTitle,
   deleteCategory,
+  categoryById,
 } = require("../../controllers/category.controller");
 
 router.get("/", allCategories); // get all categories
+router.get("/id/:id", categoryById); // get categories by title
 router.get("/title/:title", categoriesByTitle); // get categories by title
 router.post("/create", upload.single("image"), createCategory); // create category
 router.delete("/delete/:id", deleteCategory); // delete category
