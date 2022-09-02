efafacd784bb57e2e04547d64f729e41bab6037a
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-09-02 14:54:31 +0200

    changed to icon btns

--- frontend/src/components/createCategory.js
+++ frontend/src/components/createCategory.js
@@ -131,15 +131,37 @@ export default function CreateCategory() {
                       </Link>
                     </td>
                     <td>{category.subCategories.length}</td>
-                    <td className="flex justify-start items-start gap-5">
-                      <button className="btn btn-primary btn-sm btn-round-s m-0">
-                        Edit
+                    <td className="flex justify-start items-center gap-2 h-full">
+                      <button className="btn btn-primary btn-round-s btn-icon btn-icon--sm m-0">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-pencil-square"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                          <path
+                            fill-rule="evenodd"
+                            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
+                          />
+                        </svg>
                       </button>
                       <button
-                        className="btn btn-error btn-sm btn-round-s m-0"
                         onClick={() => deleteCategory(category._id)}
+                        className="btn btn-error btn-round-s btn-icon btn-icon--sm m-0"
                       >
-                        Delete
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-trash2"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M14 3a.702.702 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671L2.037 3.225A.703.703 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2zM3.215 4.207l1.493 8.957a1 1 0 0 0 .986.836h4.612a1 1 0 0 0 .986-.836l1.493-8.957C11.69 4.689 9.954 5 8 5c-1.954 0-3.69-.311-4.785-.793z" />
+                        </svg>
                       </button>
                     </td>
                   </tr>
