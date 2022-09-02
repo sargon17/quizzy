a595f9d8fe4b31bbfc8a271e55e5b715dbbe67d2
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-09-02 14:52:32 +0200

    updating sub-cat api on call by cat id

--- backend/controllers/sub_category.controller.js
+++ backend/controllers/sub_category.controller.js
@@ -20,22 +20,14 @@ exports.getSubCategories = async (req, res) => {
 exports.subCategoriesByCategory = async (req, res) => {
   let subCategories;
   try {
-    let c = await SubCategory.find({ category: req.params.categoryID })
-      .populate("category")
+    let s = await SubCategory.find({ category: req.params.categoryID })
+      .populate({
+        path: "category",
+        select: "title",
+      })
+      .sort({ createdAt: -1 })
       .exec();
-
-    subCategories = c.map((subCategory) => {
-      return {
-        id: subCategory.id,
-        title: subCategory.title,
-        image: subCategory.imagePath,
-        category: subCategory.category,
-      };
-    });
-    res.status(200).json({
-      message: "Sub Categories retrieved successfully!",
-      subCategories: subCategories,
-    });
+    res.status(200).json(s);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
@@ -83,8 +75,13 @@ exports.createSubCategory = async (req, res) => {
     category: req.body.categoryID,
   });
 
+  let category = await Category.findById(req.body.categoryID);
+
   try {
     await subCategory.save();
+
+    category.subCategories.push(subCategory._id);
+    await category.save();
     res.status(200).json({
       message: "Sub Category created successfully!",
       subCategory: subCategory,
