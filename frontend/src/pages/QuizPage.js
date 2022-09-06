18a9518b51cabcea3a471a00942731a9b05b541e
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-09-06 19:02:29 +0200

    like btn

--- frontend/src/pages/QuizPage.js
+++ frontend/src/pages/QuizPage.js
@@ -7,6 +7,7 @@ import { useCookies } from "react-cookie";
 import { Link } from "react-router-dom";
 
 import axios from "axios";
+import { getUser } from "gh-pages/lib/util";
 
 export default function QuizPage() {
   const [cookies, setCookie, removeCookie] = useCookies(["user"]);
@@ -112,6 +113,23 @@ export default function QuizPage() {
     );
     setCorrectPercentage(percentage);
     saveQuizStats(percentage);
+  };
+
+  const [userLiked, setUserLiked] = useState(false);
+
+  // useEffect(() => {
+  //   handleLike();
+  // }, [userLiked]);
+
+  const handleLike = () => {
+    axios
+      .put(`http://localhost:5000/api/quiz/like/${quizID}`, {
+        user: cookies.user._id || null,
+        like: true,
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   };
 
   return (
@@ -256,6 +274,37 @@ export default function QuizPage() {
               <div className="question-card__results">
                 <h1>Results</h1>
                 <h2>You got {correctPercentage}% correct</h2>
+                <div className="flex justify-center items-center gap-5">
+                  <p>Did you liked this quiz?</p>
+                  <div onClick={handleLike} className="like-btn">
+                    {userLiked ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-heart-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-heart"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                      </svg>
+                    )}
+                  </div>
+                </div>
               </div>
             )}
           </div>
